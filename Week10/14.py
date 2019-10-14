a03e98bc10b98fa6bc0fece4d01c9505db44a25e
Turtle Art Gallery <mr.cheesey10@gmail.com>
2019-10-14 08:44:24 -0500

    weeeee

--- Week10/14.py
+++ Week10/14.py
@@ -4,4 +4,17 @@ import math
 print(12/6)
 print(355/113)
 
-print(9001 / (2206 * math.sqrt() ))
+print(24 / (6 * math.sqrt(2)))
+
+def archimedes(numSides):
+    innerAngleB = 360.0 / numSides
+    halfAngleA = innerAngleB / 2
+    oneHalfSideS = math.sin(math.radians(halfAngleA))
+    sideS = oneHalfSideS * 2
+    polygonCircumference = numSides * sideS
+    pi = polygonCircumference / 2
+    return pi
+
+
+print(archimedes(8))
+print(archimedes(32))

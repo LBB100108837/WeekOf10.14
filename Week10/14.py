506273763cf16931d43b2f0b2003370ea2c21512
Turtle Art Gallery <mr.cheesey10@gmail.com>
2019-10-14 08:56:28 -0500

    weeeee

--- Week10/14.py
+++ Week10/14.py
@@ -18,3 +18,16 @@ def archimedes(numSides):
 
 print(archimedes(8))
 print(archimedes(32))
+
+
+
+
+for sides in range(8, 100, 8):
+    print(sides, archimedes(sides))
+
+# Experiment with the loop above alongside the actual value of Pi. How many
+# sides does it take to make the two close
+
+
+# Modify the archimedes function to take the radius of the circle as a parameter
+# Can you get a better answer more quickly using a larger circle?

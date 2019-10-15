3bab8966774dfde7abbf461a6c35bdd8c512bd6a
Turtle Art Gallery <mr.cheesey10@gmail.com>
2019-10-15 08:32:42 -0500

    weeeee

--- Week10/14.py
+++ Week10/14.py
@@ -13,7 +13,7 @@ def archimedes(numSides):
     return pi
 
 
-print(archimedes(100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000))
+print(archimedes(100000000000))
 
 
 # This is pi 3.14159265359
@@ -22,7 +22,7 @@ print(archimedes(100000000000000000000000000000000000000000000000000000000000000
 
 # Experiment with the loop above alongside the actual value of Pi. How many
 # sides does it take to make the two close
-# IT TAKES MORE SIDES
+# IT TAKES MORE SIDES ex; 100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
 
 # Modify the archimedes function to take the radius of the circle as a parameter
 # Can you get a better answer more quickly using a larger circle?
@@ -36,4 +36,31 @@ def archimedes(numSides):
     polygonCircumference = numSides * sideS
     pi = polygonCircumference / 8
     return pi
-print(archimedes(100000000000))
+print(archimedes(1000000000))
+
+
+
+
+# Accumulators
+
+acc = 0
+for x in range(1, 6):
+    acc = acc + x
+
+print(acc)
+
+
+# compute the sum of the first 100 even numbers
+# compute the sum of the first 50 odd numbers
+# compute the average of the first 100 odd numbers
+# write a function that returns the average of the first N numbers where
+#   N is a parameter
+# write a function called factorial that computes the product of the first N
+#   numbers, where N is a parameter
+# each number in the Fibonacci sequence is the sum of the previous two numbers
+#   The first two numbers in the sequence are 1 and 1. Compute the 10th
+#   Fibonacci number
+# Write a function to compute the Nth Fibonacci number, where N is a Parameter.
+#   You may assume that N will be greater than or equal to 3.
+#
+
